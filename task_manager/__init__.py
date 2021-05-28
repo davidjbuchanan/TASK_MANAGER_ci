964412179713579b12507acfe4ee5c55cbb6d871
davidjbuchanan <davidjbuchanan@gmail.com>
2021-05-28 12:48:35 +0100

    added routes and env file

--- task_manager/__init__.py
+++ task_manager/__init__.py
@@ -1,11 +1,17 @@
+import os
 from flask import Flask
 from .main.routes import main
 from .extensions import mongo
+if os.path.exists("env.py"):
+    import env
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['MONGO_URI'] = 'mongodb+srv://oxymichael2:<password>@cluster0.xlavz.mongodb.net/task_manager?retryWrites=true&w=majority'
+    app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+    app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+    app.secret_key = os.environ.get("SECRET_KEY")
+
     mongo.init_app(app)
 
     app.register_blueprint(main)
--- task_manager/main/routes.py
+++ task_manager/main/routes.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
-
+from flask import (
+    Blueprint, flash, render_template, 
+    redirect, request, session, url_for)
+from bson.objectid import ObjectId
+from task_manager.extensions import mongo
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@main.route('/get_tasks')
 def index():
-    return render_template('index.html')
+    tasks = mongo.db.tasks.find()
+    return render_template('tasks.html', tasks=tasks)
